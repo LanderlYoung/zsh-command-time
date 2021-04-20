2e27f2f14d29315780c33d448bf8a343094c2cad
yangchao12 <yangchao12@kuaishou.com>
2021-04-20 20:28:58 +0800

    customise

--- command-time.plugin.zsh
+++ command-time.plugin.zsh
@@ -10,29 +10,31 @@ _command_time_preexec() {
     done
   fi
 
-  timer=${timer:-$SECONDS}
-  ZSH_COMMAND_TIME_MSG=${ZSH_COMMAND_TIME_MSG-"Time: %s"}
-  ZSH_COMMAND_TIME_COLOR=${ZSH_COMMAND_TIME_COLOR-"white"}
-  export ZSH_COMMAND_TIME=""
+  __zsh_extesion_command_time_var_timer=${__zsh_extesion_command_time_var_timer:-$SECONDS}
+  ZSH_COMMAND_TIME_COLOR=${ZSH_COMMAND_TIME_COLOR:-"cyan"}
 }
 
 _command_time_precmd() {
-  if [ $timer ]; then
-    timer_show=$(($SECONDS - $timer))
-    if [ -n "$TTY" ] && [ $timer_show -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
-      export ZSH_COMMAND_TIME="$timer_show"
-      if [ ! -z ${ZSH_COMMAND_TIME_MSG} ]; then
-        zsh_command_time
-      fi
+  if [ $__zsh_extesion_command_time_var_timer ]; then
+    local time_diff=$(($SECONDS - $__zsh_extesion_command_time_var_timer))
+    if [ -n "$TTY" ] && [ $time_diff -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
+      zsh_command_time "$time_diff"
     fi
-    unset timer
   fi
+  unset __zsh_extesion_command_time_var_timer
 }
 
 zsh_command_time() {
+  local ZSH_COMMAND_TIME=$1
   if [ -n "$ZSH_COMMAND_TIME" ]; then
-    timer_show=$(printf '%dh:%02dm:%02ds\n' $(($ZSH_COMMAND_TIME/3600)) $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
-    print -P "%F{$ZSH_COMMAND_TIME_COLOR}$(printf "${ZSH_COMMAND_TIME_MSG}\n" "$timer_show")%f"
+      if [[ ${ZSH_COMMAND_TIME} -ge $((60 * 60)) ]]; then
+        local msg=$(printf '%dh:%dm:%ds' $(($ZSH_COMMAND_TIME/3600)) $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
+      elif [[ ${ZSH_COMMAND_TIME} -ge $((60)) ]]; then
+        msg=$(printf '%dm:%ds' $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
+      else
+        msg=$(printf '%ds' $(($ZSH_COMMAND_TIME)))
+      fi
+      export RPROMPT="%F{${ZSH_COMMAND_TIME_COLOR}}${msg}" #%{$reset_color%}"`
   fi
 }
 
