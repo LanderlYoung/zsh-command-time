a2a6d9a0af7b630212e401cf929d734c83ad297f
yangchao12 <yangchao12@kuaishou.com>
2022-02-07 09:59:00 +0800

    update

--- command-time.plugin.zsh
+++ command-time.plugin.zsh
@@ -1,9 +1,9 @@
 _command_time_preexec() {
   # check excluded
   if [ -n "$ZSH_COMMAND_TIME_EXCLUDE" ]; then
-    cmd="$1"
+    local cmd="$1"
     for exc ($ZSH_COMMAND_TIME_EXCLUDE) do;
-      if [ "$(echo $cmd | grep -c "$exc")" -gt 0 ]; then
+      if [[ "$cmd" == "$exc" ]]; then
         # echo "command excluded: $exc"
         return
       fi
@@ -15,28 +15,31 @@ _command_time_preexec() {
 }
 
 _command_time_precmd() {
-  if [ $__zsh_extesion_command_time_var_timer ]; then
-    local time_diff=$(($SECONDS - $__zsh_extesion_command_time_var_timer))
-    if [ -n "$TTY" ] && [ $time_diff -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
-      zsh_command_time "$time_diff"
-    fi
-  fi
+  local timer=$__zsh_extesion_command_time_var_timer
   unset __zsh_extesion_command_time_var_timer
+  if [ $timer ]; then
+    local time_diff=$(($SECONDS - $timer))
+    zsh_command_time "$time_diff"
+  fi
 }
 
 zsh_command_time() {
-  local ZSH_COMMAND_TIME=$1
-  if [ -n "$ZSH_COMMAND_TIME" ]; then
-      if [[ ${ZSH_COMMAND_TIME} -ge $((60 * 60)) ]]; then
-        local msg=$(printf '%dh:%dm:%ds' $(($ZSH_COMMAND_TIME/3600)) $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
-      elif [[ ${ZSH_COMMAND_TIME} -ge $((60)) ]]; then
-        msg=$(printf '%dm:%ds' $(($ZSH_COMMAND_TIME%3600/60)) $(($ZSH_COMMAND_TIME%60)))
+  local time_diff=$1
+  if [ $time_diff -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
+      if [[ ${time_diff} -ge $((60 * 60)) ]]; then
+        local msg=$(printf '%dh:%dm:%ds' $(($time_diff/3600)) $(($time_diff%3600/60)) $(($time_diff%60)))
+      elif [[ ${time_diff} -ge $((60)) ]]; then
+        local msg=$(printf '%dm:%ds' $(($time_diff%3600/60)) $(($time_diff%60)))
       else
-        msg=$(printf '%ds' $(($ZSH_COMMAND_TIME)))
+        local msg=$(printf '%ds' $(($time_diff)))
       fi
-      export RPROMPT="%F{${ZSH_COMMAND_TIME_COLOR}}${msg}" #%{$reset_color%}"`
+      RPROMPT="%F{${ZSH_COMMAND_TIME_COLOR}}${msg}"
+  else
+      # clear previous right prompt
+      RPROMPT=""
   fi
 }
 
 precmd_functions+=(_command_time_precmd)
 preexec_functions+=(_command_time_preexec)
+
